95670a3eb71704af0b17c48e0703ce129a45bbf5
ansike <ansike@bytedance.com>
2024-08-26 21:44:12 +0800

    fix: 数据为空时文案颜色

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,7 +7,6 @@ export default function App() {
     <View style={styles.container}>
       <RNNotificationBar
         style={{
-          backgroundColor: 'red',
           marginTop: 100,
         }}
         notices={[
